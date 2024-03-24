1b7605d32c625dc9bff3d60e888b9081a5cb0218
Niwa97 <48857497+Niwa97@users.noreply.github.com>
2024-03-24 18:14:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ template <std::size_t, typename T>
 using tensor_shape = T;
 
 template <typename T, std::size_t... N>
-class DeltaAdditiveFunction
+class TensorFunction
 {
 public:
-    explicit DeltaAdditiveFunction(std::vector<T> data) : functionValues(std::move(data))
+    explicit TensorFunction(std::vector<T> data) : functionValues(std::move(data))
     {
         assert(functionValues.size() == (1 * ... * N));
     }
@@ -25,28 +25,52 @@ public:
     {
         return operator()({ {indexes...} });
     }
+
     bool isOneAdditive()
     {
-        if(functionValues.size() != 18)
+        if(functionValues.size() != 27)
         {
             return false;
         }
-        if( (std::abs(functionValues[0]) <= 1) && (std::abs(functionValues[1]) <= 1) && (std::abs(functionValues[2]) <= 1) //X_1', X_2', X_3'
-        && (std::abs(functionValues[0] + functionValues[1] - functionValues[3]) <= 1) //X_1' u X_2'
-        && (std::abs(functionValues[0] + functionValues[2] - functionValues[4]) <= 1) //X_1' u X_3'
-        && (std::abs(functionValues[1] + functionValues[2] - functionValues[5]) <= 1) //X_2' u X_3'
-        && (std::abs(functionValues[6] + functionValues[7] - functionValues[9]) <= 1) //X_1 u X_2
-        && (std::abs(functionValues[6] + functionValues[8] - functionValues[10]) <= 1) //X_1 u X_3
-        && (std::abs(functionValues[7] + functionValues[8] - functionValues[11]) <= 1) //X_2 u X_3
-        && (std::abs(functionValues[2] + functionValues[6] - functionValues[13]) <= 1) //X_1 u X_3'
-        && (std::abs(functionValues[1] + functionValues[6] - functionValues[12]) <= 1) //X_1 u X_2'
-        && (std::abs(functionValues[2] + functionValues[7] - functionValues[14]) <= 1) //X_2 u X_3'
-        && (std::abs(functionValues[2] + functionValues[9] - functionValues[15]) <= 1)   //X_1 u X_2 u X_3'
-        && (std::abs(functionValues[1] + functionValues[11] - functionValues[16]) <= 1)  //X_1 u X_3 U X_2'
-        && (std::abs(functionValues[0] + functionValues[11] - functionValues[17]) <= 1)  //X_2 u X_2 u X_1'
-        && (std::abs(functionValues[6] + functionValues[14] - functionValues[15]) <= 1)  //X_1 u X_2 u X_3'
-        && (std::abs(functionValues[7] + functionValues[13] - functionValues[15]) <= 1)  //X_1 u X_2 u X_3'
-        && (std::abs(functionValues[8] + functionValues[12] - functionValues[16]) <= 1)) //X_1 u X_3 U X_2'
+        if( (functionValues[0] == 1) && (std::abs(functionValues[1]) <= 1)
+        && (std::abs(functionValues[2]) <= 1) && (std::abs(functionValues[3]) <= 1)
+        && (std::abs(functionValues[1] + functionValues[2] - functionValues[4]) <= 1) //X_1' u X_2'
+        && (std::abs(functionValues[1] + functionValues[3] - functionValues[5]) <= 1) //X_1' u X_3'
+        && (std::abs(functionValues[2] + functionValues[3] - functionValues[6]) <= 1) //X_2' u X_3'
+        && (std::abs(functionValues[7] + functionValues[8] - functionValues[10]) <= 1) //X_1 u X_2
+        && (std::abs(functionValues[7] + functionValues[9] - functionValues[11]) <= 1) //X_1 u X_3
+        && (std::abs(functionValues[8] + functionValues[9] - functionValues[12]) <= 1) //X_2 u X_3
+        && (std::abs(functionValues[2] + functionValues[7] - functionValues[13]) <= 1) //X_1 u X_2'
+        && (std::abs(functionValues[3] + functionValues[7] - functionValues[14]) <= 1) //X_1 u X_3'
+        && (std::abs(functionValues[1] + functionValues[8] - functionValues[15]) <= 1) //X_2 u X_1'
+        && (std::abs(functionValues[3] + functionValues[8] - functionValues[16]) <= 1) //X_2 u X_3'
+        && (std::abs(functionValues[1] + functionValues[9] - functionValues[17]) <= 1) //X_3 u X_1'
+        && (std::abs(functionValues[2] + functionValues[9] - functionValues[18]) <= 1) //X_3 u X_2'
+        && (std::abs(functionValues[7] + functionValues[16] - functionValues[19]) <= 1)  //X_1 u X_2 u X_3'
+        && (std::abs(functionValues[8] + functionValues[14] - functionValues[19]) <= 1)  //X_1 u X_2 u X_3'
+        && (std::abs(functionValues[3] + functionValues[10] - functionValues[19]) <= 1)  //X_1 u X_2 u X_3'
+        && (std::abs(functionValues[7] + functionValues[18] - functionValues[20]) <= 1)  //X_1 u X_3 u X_2'
+        && (std::abs(functionValues[9] + functionValues[13] - functionValues[20]) <= 1)  //X_1 u X_3 u X_2'
+        && (std::abs(functionValues[2] + functionValues[11] - functionValues[20]) <= 1)  //X_1 u X_3 u X_2'
+        && (std::abs(functionValues[8] + functionValues[17] - functionValues[21]) <= 1)  //X_2 u X_3 u X_1'
+        && (std::abs(functionValues[9] + functionValues[15] - functionValues[21]) <= 1)  //X_2 u X_3 u X_1'
+        && (std::abs(functionValues[1] + functionValues[12] - functionValues[21]) <= 1)  //X_2 u X_3 u X_1'
+        && (std::abs(functionValues[1] + functionValues[6] - functionValues[22]) <= 1)   //X_1' u X_2' u X_3'
+        && (std::abs(functionValues[2] + functionValues[5] - functionValues[22]) <= 1)   //X_1' u X_2' u X_3'
+        && (std::abs(functionValues[3] + functionValues[4] - functionValues[22]) <= 1)   //X_1' u X_2' u X_3'
+        && (std::abs(functionValues[7] + functionValues[6] - functionValues[23]) <= 1)   //X_1 u X_2' u X_3'
+        && (std::abs(functionValues[2] + functionValues[14] - functionValues[23]) <= 1)  //X_1 u X_2' u X_3'
+        && (std::abs(functionValues[3] + functionValues[13] - functionValues[23]) <= 1)  //X_1 u X_2' u X_3'
+        && (std::abs(functionValues[8] + functionValues[5] - functionValues[24]) <= 1)   //X_2 u X_1' u X_3'
+        && (std::abs(functionValues[1] + functionValues[16] - functionValues[24]) <= 1)  //X_2 u X_1' u X_3'
+        && (std::abs(functionValues[3] + functionValues[15] - functionValues[24]) <= 1)  //X_2 u X_1' u X_3'
+        && (std::abs(functionValues[9] + functionValues[4] - functionValues[25]) <= 1)   //X_3 u X_1' u X_2'
+        && (std::abs(functionValues[1] + functionValues[18] - functionValues[25]) <= 1)  //X_3 u X_1' u X_2'
+        && (std::abs(functionValues[2] + functionValues[17] - functionValues[25]) <= 1)  //X_3 u X_1' u X_2'
+        && (std::abs(functionValues[7] + functionValues[12] - functionValues[26]) <= 1)  //X_1 u X_2 u X_3
+        && (std::abs(functionValues[8] + functionValues[11] - functionValues[26]) <= 1)  //X_1 u X_2 u X_3
+        && (std::abs(functionValues[9] + functionValues[10] - functionValues[26]) <= 1)  //X_1 u X_2 u X_3
+        )
         {
             return true;
         }
@@ -56,7 +80,7 @@ public:
         }
     }
 
-    void printDeltaAdditive()
+    void printTensor()
     {
         typename std::vector<T>::iterator iter;
         int index = 0;
@@ -85,10 +109,11 @@ private:
 
 int main()
 {
-    std::vector<std::vector<double>> tensors;
+    std::vector<std::vector<double>> tensors_values;
     std::vector<double> V;
-    for(int i = 0; i < 18; ++i)
+    V.push_back(0.0);
+    for(int i = 0; i < 26; ++i)
         V.push_back(1.0);
-    DeltaAdditiveFunction<double, 3, 3, 2> F(V);
+    TensorFunction<double, 3, 3, 3> F(V);
     return 0;
 }
