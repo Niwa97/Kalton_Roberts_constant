3623023f40553cf9f155785337841a535cfcd6a1
niwa97 <mariusz.niwinski.pl@gmail.com>
2025-11-19 07:18:04 +0100

    Code refinements

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <array>
 #include <cassert>
 #include <vector>
+#include <ranges>
+
+constexpr std::size_t N = 23;
 
 template <std::size_t, typename T>
 using tensor_shape = T;
@@ -125,111 +128,25 @@ private:
 
 int main()
 {
+    auto to_tensor = [](const unsigned long long mask) {
+        std::array<double, 4 + N> arr{0.0, 1.0, 1.0, 1.0};
+
+        for (std::size_t i = 0; i < N; ++i)
+            arr[4 + i] = (mask & (1ULL << i)) ? 3.0 : 1.0;
+
+        const std::vector<double> vec(arr.begin(), arr.end());
+        return TensorFunction<double,3,3,3>(vec);
+    };
+
+    constexpr auto all_masks = std::views::iota(0ULL, (1ULL << N));
     std::vector<TensorFunction<double,3,3,3>> one_additive;
-    int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w;
-    for(a = 1; a < 4; a+=2)
+
+    for (const auto mask : all_masks)
     {
-        for(b = 1; b < 4; b+=2)
-        {
-            for(c = 1; c < 4; c+=2)
-            {
-                for(d = 1; d < 4; d+=2)
-                {
-                    for(e = 1; e < 4; e+=2)
-                    {
-                        for(f = 1; f < 4; f+=2)
-                        {
-                            for(g = 1; g < 4; g+=2)
-                            {
-                                for(h = 1; h < 4; h+=2)
-                                {
-                                    for(i = 1; i < 4; i+=2)
-                                    {
-                                        for(j = 1; j < 4; j+=2)
-                                        {
-                                            for(k = 1; k < 4; k+=2)
-                                            {
-                                                for(l = 1; l < 4; l+=2)
-                                                {
-                                                    for(m = 1; m < 4; m+=2)
-                                                    {
-                                                        for(n = 1; n < 4; n+=2)
-                                                        {
-                                                            for(o = 1; o < 4; o+=2)
-                                                            {
-                                                                for(p = 1; p < 4; p+=2)
-                                                                {
-                                                                    for(q = 1; q < 4; q+=2)
-                                                                    {
-                                                                        for(r = 1; r < 4; r+=2)
-                                                                        {
-                                                                            for(s = 1; s < 4; s+=2)
-                                                                            {
-                                                                                for(t = 1; t < 4; t+=2)
-                                                                                {
-                                                                                    for(u = 1; u < 4; u+=2)
-                                                                                    {
-                                                                                        for(v = 1; v < 4; v+=2)
-                                                                                        {
-                                                                                            for(w = 1; w < 4; w+=2)
-                                                                                            {
-                                                                                                std::vector<double> vec;
-                                                                                                vec.push_back(0.0);
-                                                                                                vec.push_back(1.0);
-                                                                                                vec.push_back(1.0);
-                                                                                                vec.push_back(1.0);
-                                                                                                vec.push_back(a);
-                                                                                                vec.push_back(b);
-                                                                                                vec.push_back(c);
-                                                                                                vec.push_back(d);
-                                                                                                vec.push_back(e);
-                                                                                                vec.push_back(f);
-                                                                                                vec.push_back(g);
-                                                                                                vec.push_back(h);
-                                                                                                vec.push_back(i);
-                                                                                                vec.push_back(j);
-                                                                                                vec.push_back(k);
-                                                                                                vec.push_back(l);
-                                                                                                vec.push_back(m);
-                                                                                                vec.push_back(n);
-                                                                                                vec.push_back(o);
-                                                                                                vec.push_back(p);
-                                                                                                vec.push_back(q);
-                                                                                                vec.push_back(r);
-                                                                                                vec.push_back(s);
-                                                                                                vec.push_back(t);
-                                                                                                vec.push_back(u);
-                                                                                                vec.push_back(v);
-                                                                                                vec.push_back(w);
-                                                                                                TensorFunction<double, 3, 3, 3> fun(vec);
-                                                                                                if(fun.isOneAdditive())
-                                                                                                {
-                                                                                                    one_additive.push_back(fun);
-                                                                                                }
-                                                                                            }
-                                                                                        }
-                                                                                    }
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        if (auto f = to_tensor(mask); f.isOneAdditive())
+            one_additive.push_back(std::move(f));
     }
-    
+
     std::cout << "Number of one-additive functions " << one_additive.size() << std::endl;
     return 0;
 }
